a54ff29cc6dba47fbf02cd3aede9389784038f1a
Dabin_Ki <rlekqls12@naver.com>
2022-11-02 23:10:51 +0900

    아이템 클래스 완성, 아이템 엘레멘트 정리 및 팝업 임시 추가 및 아이템과 데이터 연동

--- index.js
+++ index.js
@@ -8,11 +8,60 @@ const globalData = {
 };
 
 window.addEventListener("DOMContentLoaded", () => {
+  // connect image size width
+  const imageSizeWidth = document.querySelector("#image-size-width");
+  imageSizeWidth.value = globalData.image.width;
+  imageSizeWidth.addEventListener("change", (event) => {
+    const width = event.target.value;
+    globalData.image.width = width;
+    syncCanvasSize();
+  });
+
+  // connect image size width
+  const imageSizeHeight = document.querySelector("#image-size-height");
+  imageSizeHeight.value = globalData.image.height;
+  imageSizeHeight.addEventListener("change", (event) => {
+    const height = event.target.value;
+    globalData.image.height = height;
+    syncCanvasSize();
+  });
+
+  // sync canvas size
+  syncCanvasSize();
+
   // add item - event listener
   const addItemButton = document.querySelector("#item-add");
   addItemButton.addEventListener("click", () => {
     // TODO: 팝업 보여주고 확인 누르면 아래 코드 실행해야함 new Item(type);
-    const newItem = new Item();
+    const newItem = new Item("barcode");
     globalData.itemList.push(newItem);
   });
+
+  // draw canvas
+  drawCanvas();
 });
+
+function drawCanvas() {
+  syncCanvasSize();
+  const canvas = document.querySelector("#main-canvas");
+  const context = canvas.getContext("2d");
+
+  const offset = devicePixelRatio || 1;
+  const image = globalData.image;
+  context.clearRect(0, 0, image.width * offset, image.height * offset);
+
+  // draw items
+
+  requestAnimationFrame(drawCanvas);
+}
+
+function syncCanvasSize() {
+  const canvas = document.querySelector("#main-canvas");
+  const offset = devicePixelRatio || 1;
+
+  canvas.width = globalData.image.width * offset;
+  canvas.height = globalData.image.height * offset;
+
+  canvas.style.width = `${globalData.image.width}px`;
+  canvas.style.height = `${globalData.image.height}px`;
+}
--- item.js
+++ item.js
@@ -1,10 +1,25 @@
-class Item {
-  id = null;
-  element = null;
-  type = null;
-  data = null;
+const initType = "text";
+const initData = {
+  text: { x: 0, y: 0, text: "", fontSize: 14 },
+  barcode: { x: 0, y: 0, data: "", type: "" },
+  qr: { x: 0, y: 0, data: "" },
+};
 
-  constructor(type = "text", data = { x: 0, y: 0, text: "", fontSize: 14 }) {
+class Item {
+  id = null; // random
+  element = null; // dom element
+  type = null; // text, barcode, qr
+  data = null; // item data
+
+  constructor(type, data) {
+    // default type and data
+    if (Boolean(type) === false) {
+      type = initType;
+    }
+    if (Boolean(data) === false) {
+      data = initData[type];
+    }
+
     // type available check
     const availableTypes = ["text", "barcode", "qr"];
     if (availableTypes.includes(type) === false) {
@@ -12,17 +27,7 @@ class Item {
     }
 
     // set id
-    this.id = Math.random().toString(16).slice(2);
-
-    // clone item element
-    const originalItem = document.querySelector('div[data-id="item"]');
-    this.element = originalItem.cloneNode(true);
-    this.element.removeAttribute("data-id");
-    this.element.classList.add(`item-type-${type}`);
-
-    // append item
-    const itemListDOM = document.querySelector("#item-list");
-    itemListDOM.appendChild(this.element);
+    this.id = Math.random().toString(16).slice(2).toUpperCase();
 
     // set type
     this.type = type;
@@ -30,11 +35,41 @@ class Item {
     // set data
     this.data = data;
 
+    // clone item element
+    const originalItem = document.querySelector('div[data-id="item"]');
+    this.element = originalItem.cloneNode(true);
+    this.element.setAttribute("data-id", `item#${this.id}`);
+    this.element.classList.add(`item-type-${type}`);
+
+    // init data element
+    this.initDataElement();
+
+    // append item
+    const itemListDOM = document.querySelector("#item-list");
+    itemListDOM.appendChild(this.element);
+
     // init event listener
-    this.#initEventListener();
+    this.initEventListener();
   }
 
-  #initEventListener() {
+  delete() {
+    // remove element
+    this.element.remove();
+
+    // remove item in itemlist
+    const $id = this.id;
+    const itemIndex = globalData.itemList.findIndex((item) => item.id === $id);
+    globalData.itemList.splice(itemIndex, 1);
+
+    // reset value
+    this.id = null;
+    this.element = null;
+    this.type = null;
+    this.data = null;
+  }
+
+  initEventListener() {
+    // ------------------------- click event
     this.element.addEventListener("click", (event) => {
       // get item dom
       const currentItemDOM = event.currentTarget;
@@ -47,7 +82,7 @@ class Item {
         dataID = targetDOM.getAttribute("data-id");
       }
 
-      // data-id = event-layout-toggle
+      // layout toggle
       if (dataID === "event-layout-toggle") {
         // toggle visible event layout
         const itemControlLayout = currentItemDOM.querySelector(
@@ -59,10 +94,134 @@ class Item {
         const span = targetDOM.querySelector("span");
         span.textContent = span.textContent === "▼" ? "▲" : "▼";
       }
+
+      // delete
+      if (dataID === "event-delete") {
+        this.delete();
+      }
+    });
+
+    // ------------------------- change event
+    this.element.addEventListener("change", (event) => {
+      // get item dom
+      const currentItemDOM = event.currentTarget;
+      // get changed dom data-id
+      const changedDOM = event.target;
+      let targetDOM = changedDOM;
+      let dataID = targetDOM.getAttribute("data-id");
+      if (dataID === null) {
+        targetDOM = changedDOM.parentNode;
+        dataID = targetDOM.getAttribute("data-id");
+      }
+
+      const value = targetDOM.value;
+      const number = targetDOM?.valueAsNumber;
+
+      // data
+      if (dataID === "item-input-data") {
+        if (this.type === "text") {
+          this.data.text = value;
+        }
+        if (this.type === "barcode") {
+          this.data.data = value;
+        }
+        if (this.type === "qr") {
+          this.data.data = value;
+        }
+      }
+
+      // x
+      if (dataID === "item-input-x") {
+        this.data.x = number;
+      }
+
+      // y
+      if (dataID === "item-input-y") {
+        this.data.y = number;
+      }
+
+      // font size
+      if (dataID === "item-input-font-size") {
+        this.data.fontSize = number;
+      }
+
+      // barcode-type
+      if (dataID === "item-select-barcode-type") {
+        this.data.type = value;
+      }
     });
   }
 
-  modify() {}
-  delete() {}
-  editData(key, value) {}
+  initDataElement() {
+    const $id = this.id.slice(0, 4);
+    const $type = this.type;
+    const $element = this.element;
+    const $data = this.data;
+
+    // get item child element function
+    const getElement = (tag, dataId) =>
+      $element.querySelector(`${tag}[data-id="${dataId}"]`);
+
+    // item inserted event
+    const itemListDOM = document.querySelector("#item-list");
+    itemListDOM.addEventListener(
+      "DOMNodeInserted",
+      function eventFunction(event) {
+        // check my event
+        if (event.target !== $element) return;
+
+        // data x
+        getElement("input", "item-input-x").value = $data.x;
+
+        // data y
+        getElement("input", "item-input-y").value = $data.y;
+
+        // -------------------------  text type
+        if ($type === "text") {
+          // type text
+          getElement("span", "item-type-text").textContent = `Text#${$id}`;
+
+          // data text
+          getElement("input", "item-input-data").value = $data.text;
+
+          // font size
+          getElement("input", "item-input-font-size").value = $data.fontSize;
+
+          // remove other type element
+          getElement("div", "item-layout-barcode-type").remove();
+        }
+
+        // -------------------------  barcode type
+        if ($type === "barcode") {
+          // type text
+          getElement("span", "item-type-text").textContent = `Barcode#${$id}`;
+
+          // data text
+          getElement("input", "item-input-data").value = $data.data;
+
+          // barcode type
+          getElement("select", "item-select-barcode-type").value = $data.type;
+
+          // remove other type element
+          getElement("div", "item-layout-font-size").remove();
+        }
+
+        // -------------------------  qr type
+        if ($type === "qr") {
+          // type text
+          getElement("span", "item-type-text").textContent = `QR#${$id}`;
+
+          // data text
+          getElement("input", "item-input-data").value = $data.data;
+
+          // remove other type element
+          getElement("div", "item-layout-font-size").remove();
+          getElement("div", "item-layout-barcode-type").remove();
+        }
+
+        // remove event
+        itemListDOM.removeEventListener("DOMNodeInserted", eventFunction);
+      }
+    );
+  }
 }

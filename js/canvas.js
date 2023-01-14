fc031412e7a18fcba8bab97b294ff41024364c27
Ki, Dabin <rlekqls12@naver.com>
2023-01-14 10:45:37 +0000

    canvas edge 그리는 로직 개선

--- js/canvas.js
+++ js/canvas.js
@@ -48,37 +48,37 @@ function drawCanvas() {
   context.fillStyle = "white";
   context.fillRect(canvasRect.x, canvasRect.y, canvasRect.w, canvasRect.h);
 
-  // get canvas edge rects
-  const {
-    c_edge_left,
-    c_edge_right,
-    c_edge_top,
-    c_edge_bottom,
-    c_edge_left_top,
-    c_edge_left_bottom,
-    c_edge_right_top,
-    c_edge_right_bottom,
-  } = getCanvasEdge();
-
-  // canvas edge matrix mapping
-  const CANVAS_EDGE_MATRIX_MAP = {
-    [HIT_EDGE_TYPE.LEFT]: [c_edge_left],
-    [HIT_EDGE_TYPE.RIGHT]: [c_edge_right],
-    [HIT_EDGE_TYPE.TOP]: [c_edge_top],
-    [HIT_EDGE_TYPE.BOTTOM]: [c_edge_bottom],
-    [HIT_EDGE_TYPE.LEFT_TOP]: [c_edge_left_top],
-    [HIT_EDGE_TYPE.LEFT_BOTTOM]: [c_edge_left_bottom],
-    [HIT_EDGE_TYPE.RIGHT_TOP]: [c_edge_right_top],
-    [HIT_EDGE_TYPE.RIGHT_BOTTOM]: [c_edge_right_bottom],
-    [HIT_EDGE_TYPE.NONE]: null,
-  }
-  const targetEdge = CANVAS_EDGE_MATRIX_MAP[canvasInfo.hitCanvasEdge];
-
   // reset mouse pointer
   canvas.style.cursor = 'default';
 
   // draw canvas edge
-  if (targetEdge) {
+  if (canvasInfo.hitCanvasEdge !== HIT_EDGE_TYPE.NONE) {
+    // get canvas edge rects
+    const {
+      c_edge_left,
+      c_edge_right,
+      c_edge_top,
+      c_edge_bottom,
+      c_edge_left_top,
+      c_edge_left_bottom,
+      c_edge_right_top,
+      c_edge_right_bottom,
+    } = getCanvasEdge();
+  
+    // canvas edge matrix mapping
+    const CANVAS_EDGE_MATRIX_MAP = {
+      [HIT_EDGE_TYPE.LEFT]: [c_edge_left],
+      [HIT_EDGE_TYPE.RIGHT]: [c_edge_right],
+      [HIT_EDGE_TYPE.TOP]: [c_edge_top],
+      [HIT_EDGE_TYPE.BOTTOM]: [c_edge_bottom],
+      [HIT_EDGE_TYPE.LEFT_TOP]: [c_edge_left_top],
+      [HIT_EDGE_TYPE.LEFT_BOTTOM]: [c_edge_left_bottom],
+      [HIT_EDGE_TYPE.RIGHT_TOP]: [c_edge_right_top],
+      [HIT_EDGE_TYPE.RIGHT_BOTTOM]: [c_edge_right_bottom],
+      [HIT_EDGE_TYPE.NONE]: null,
+    }
+
+    const targetEdge = CANVAS_EDGE_MATRIX_MAP[canvasInfo.hitCanvasEdge];
     const targetEdgeCurosr = CANVAS_EDGE_CURSOR_MAP[canvasInfo.hitCanvasEdge];
     canvas.style.cursor = targetEdgeCurosr;
 
